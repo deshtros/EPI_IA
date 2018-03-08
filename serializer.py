54fdba1140d1a8d5112e8a1dadb6c720f0d89184
Guillaume <gsabatie@gmail.com>
2018-03-09 00:25:07 +0100

    added serizlize ans deserialize

--- serializer.py
+++ serializer.py
@@ -3,18 +3,19 @@ from enum import Enum
 passages = [{1,4},{0,2},{1,3},{2,7},{0,5,8},{4,6},{5,7},{3,6,9},{4,9},{7,8}]
 pass_ext = [{1,4},{0,2,5,7},{1,3,6},{2,7},{0,5,8,9},{4,6,1,8},{5,7,2,9},{3,6,9,1},{4,9,5},{7,8,4,6}]
 
-CHARACTER_INFOS_SERVER_INFO = ["Tuiles", "disponibles"]
+CHOOSE_CARD_TO_PLAY_SERVER_ASK = ["Tuiles", "disponibles"]
 ENABLE_POWER_SERVER_ASK = ["activer","le","pouvoir"]
 CHOOSE_POSITION_SERVER_ASK = ["positions","disponibles"]
 CHOOSE_COLOR_SERVER_ASK = ["Avec" "quelle" "couleur" "échanger"]
 
-POSSIBLE_SERVER_OUTPUT = [CHARACTER_INFOS_SERVER_INFO, ENABLE_POWER_SERVER_ASK, CHOOSE_POSITION_SERVER_ASK, CHOOSE_COLOR_SERVER_ASK]
+SERVER_ASK_WORD_ARRAY = [CHOOSE_CARD_TO_PLAY_SERVER_ASK, ENABLE_POWER_SERVER_ASK, CHOOSE_POSITION_SERVER_ASK, CHOOSE_COLOR_SERVER_ASK]
 
-class ServerOutput(Enum):
-    CHARACTER_INFOS = 0
+class ServerOutputType(Enum):
+    CHOOSE_CARD_TO_PLAY = 0
     ENABLE_POWER = 1
     CHOOSE_POSITION = 2
     CHOOSE_COLOR = 3
+    NOT_IMPLEMENTED = 4
 
 
 class Character:
@@ -34,7 +35,9 @@ class ServerMessage:
         self.shouldSendResponse = shouldSendResponse
         
     def __repr__(self):
-        return "[ServerMessage] " + str(self.content) 
+        return "[ServerMessage] content: " + str(self.content) + " messageType: " + str(self.messageType) 
+
+
 
 
 def characterFronString(line):
@@ -62,32 +65,59 @@ def checkLineWith(serverOutput, line):
             return False
     return True
 
+
 def serverOutputWordToENum(possibleServerOutput):
-    if possibleServerOutput == CHARACTER_INFOS_SERVER_INFO:
-        return ServerOutput.CHARACTER_INFOS
+    if possibleServerOutput == CHOOSE_CARD_TO_PLAY_SERVER_ASK:
+        return ServerOutputType.CHOOSE_CARD_TO_PLAY
+    else:
+        return ServerOutputType.NOT_IMPLEMENTED
 
 def serverOutputToServerMessage(line):
-    for  element in POSSIBLE_SERVER_OUTPUT:
-        if checkLineWith(element, line) == True:
-            serverOutput = serverOutputWordToENum(element)
-            if serverOutput == ServerOutput.CHARACTER_INFOS:
-                serverMessage = ServerMessage(ServerOutput.CHARACTER_INFOS, getTilePosFromLine(line), False)
-                return serverMessage 
+    serverMessage = ServerMessage(ServerOutputType.NOT_IMPLEMENTED, 0, True) 
+    for  serverAskArray in SERVER_ASK_WORD_ARRAY:
+        if checkLineWith(serverAskArray, line) == True:
+            serverOutput = serverOutputWordToENum(serverAskArray)
+            if serverOutput == ServerOutputType.CHOOSE_CARD_TO_PLAY:
+                serverMessage = ServerMessage(ServerOutputType.CHOOSE_CARD_TO_PLAY, getTilePosFromLine(line), True)
+    return serverMessage 
+
+
 
 
 
 
 class Serializer:
-    def serialize(self, linebuffer):
+
+    def __init__(self,serverOutputFilePath, serverInputfilePath):
+        self.serverOutputFilePath = serverOutputFilePath
+        self.serverInputfilePath = serverInputfilePath
+
+
+
+
+
+    def serialize(self):
         print("[serializer] serialize : start serialize")
-        for line in linebuffer:
-            print(serverOutputToServerMessage(line))
+        serverMessageArray = []
+        f = open(self.serverOutputFilePath, 'r')
+        for line in f.readlines():
+            serverMessageArray.append(serverOutputToServerMessage(line))
+        return serverMessageArray
             
     
-        
+    def serverMessageToServerInput(self, serverMessage):
+        if serverMessage.messageType == ServerOutputType.CHOOSE_CARD_TO_PLAY:
+            self.writeNumberResponse(serverMessage.content)
+        else:
+            self.writeNumberResponse(0)    
+
+    def writeNumberResponse(self, number):
+        rf = open(self.serverInputfilePath,'w')
+        rf.write(str(number))
+        rf.close()
 
 
-    def deserialize(self, linebuffer):
+
+    def deserialize(self, serverMessage):
         print("[deserialize] deserialize : start deserialize")
-        for line in linebuffer:
-            print(line)
+        self.serverMessageToServerInput(serverMessage)
