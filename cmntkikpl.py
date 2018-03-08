c17b3b2be68a90647b48641b0ea74e16f18a93af
alexan_m <marc.alexandre@epitech>
2018-03-08 21:16:59 +0100

    parsing maj casi finish

--- cmntkikpl.py
+++ cmntkikpl.py
@@ -9,34 +9,46 @@ class cmntkikpl:
         self.path = path
         self.fantome = ''
         self.role = ''
-        self.question = {}
-        self.personnage = {}
-        self.suspe = {}
-        self.Bloque = {}
+        self.question = []
+        self.personnage = []
+        self.bloque = []
+        self.tour = 0
+        self.score = ''
+        self.ombre = 0
+        self.info_line = 0
 
     def get_role(self):
-        infof = open(self.path + '/info.txt', 'r')
-        line = infof.readline()
+        infof = open(self.path + '/infos.txt', 'r')
+        line = infof.readlines()
         infof.close()
-        if line == '**************************':
+        if line[self.info_line].__contains__('**************************'):
             self.role = 'inspecteur'
+            self.info_line += 1
             return
-        if line.__contains__('!!! Le fantôme est : '):
+        if line[self.info_line].__contains__('!!! Le fantôme est : '):
             self.role = 'fantome'
-            self.fantome = line[len('!!! Le fantôme est : '):]
-            line = infof.readline()
+            self.info_line += 1
+            self.fantome = line[self.info_line][len('!!! Le fantôme est : '):]
+            self.info_line += 1
             return
         print('no role found !')
         exit()
 
+    def parse_pos(self, elem):
+        elem.split('-')
+
     def get_context(self):
-        infof = open(self.path + '/info.txt', 'r')
-        lines = infof.readlines()
+        infof = open(self.path + '/infos.txt', 'r')
+        line = infof.readlines()
         infof.close()
-        self.Bloque += lines[lines.:]
-        infof = open(self.path + '/info.txt', 'r')
-        lines = infof.readlines()
-        infof.close()
+        lineparsed = line[self.info_line].split(' ')
+        self.tour = lineparsed[0].split(':')[1]
+        self.score = lineparsed[1].split(':')[1]
+        self.ombre = lineparsed[2].split(':')[1]
+        self.bloque = line[self.info_line].split(':')[4]
+        self.info_line += 1
+        self.personnage = line[self.info_line].split(' ')
+        self.info_line += 1
 
     def get_question(self):
         qf = open(self.path + '/questions.txt', 'r')
@@ -46,19 +58,41 @@ class cmntkikpl:
 
     def write_response(self):
         rf = open(self.path + '/reponses.txt', 'w')
+        rf.close()
+
+    def update_info(self):
+        return
+
+    def fantome_turn(self):
+        if self.role != 'fantome':
+            self.update_info()
+            return
+
+    def inspecteur_turn(self):
+        if self.role != 'inspecteur':
+            self.update_info()
+            return
+
 
     def start(self):
         fini = False
         self.get_role()
         self.get_context()
         while fini:
-            self.get_question()
-            self.write_response()
-            infof = open(self.path + '/info.txt', 'r')
-            lines = infof.readlines()
+            infof = open(self.path + '/infos.txt', 'r')
+            line = infof.readlines()
+            infof.close()
+            self.info_line += 1
+            if line[self.info_line].__contains__('Tour de le fantome'):
+                self.fantome_turn()
+            else:
+                self.inspecteur_turn()
+            infof = open(self.path + '/infos.txt', 'r')
+            lines = infof.readlines(self.info_line)
+            self.info_line += 1
             infof.close()
             if len(lines) > 0:
                 fini = "Score final" in lines[-1]
         print("partie finie")
 
-cmntkikpl(argv[0]).start()
+cmntkikpl(argv[1]).start()
